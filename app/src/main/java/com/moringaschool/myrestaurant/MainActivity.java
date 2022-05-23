06b53725ec603577e2db9b436ec09e201187dd17
mansamusah254 <mansamusah830@gmail.com>
2022-05-23 13:23:12 +0300

    new files

---
+++ app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
@@ -0,0 +1,28 @@
+package com.moringaschool.myrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
